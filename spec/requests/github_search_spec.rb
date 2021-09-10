f8ea25096cd5dcc6bb9d40f35645b43727c84760
Yasuhiro Yoshida <892791+YasuhiroYoshida@users.noreply.github.com>
2021-09-10 22:27:21 +0300

    Fix request specs

--- spec/requests/github_search_spec.rb
+++ spec/requests/github_search_spec.rb
@@ -1,76 +1,76 @@
-require 'rails_helper'
+require "rails_helper"
 
 include Shoulda::Matchers::ActionController
 
-RSpec.describe 'GithubFilesExplorer' do
-  context 'when params do not satisfy the search requirements' do
-    context 'with no search term and no repository name (default)' do
-      it 'should return no results' do
-        get '/', params: { search_term: '', repository_name: ''}
+RSpec.describe "GithubFilesExplorer" do
+  context "when params do not satisfy the search requirements" do
+    context "with no search term and no repository name (default)" do
+      it "should return no results" do
+        get "/", params: {search_term: "", repository_name: ""}
 
-        expect(response).to be_success
+        expect(response).to be_successful
         expect(assigns(:results)).to be_a(Array).and be_empty
         expect(assigns(:matched_words)).to be_a(Array).and be_empty
-        expect(controller).to set_flash.now[:info].to(/Enter a search term and repository name/)
+        expect(controller).not_to set_flash
       end
     end
 
-    context 'with one search term and no repository name' do
-      it 'should return no results' do
-        get '/', params: { search_term: 'abc', repository_name: ''}
+    context "with one search term and no repository name" do
+      it "should return no results" do
+        get "/", params: {search_term: "abc", repository_name: "", commit: "✔"}
 
-        expect(response).to be_success
+        expect(response).to be_successful
         expect(assigns(:results)).to be_a(Array).and be_empty
         expect(assigns(:matched_words)).to be_a(Array).and be_empty
-        expect(controller).to set_flash.now[:info].to(/Enter a search term and repository name/)
+        expect(controller).to set_flash.now[:danger].to(/Enter at least one search term and repository name to get results/)
       end
     end
 
-    context 'with one search term and one repository name' do
-      it 'should return no results' do
-        get '/', params: { search_term: '', repository_name: 'rails/rails'}
+    context "with one search term and one repository name" do
+      it "should return no results" do
+        get "/", params: {search_term: "", repository_name: "rails/rails", commit: "✔"}
 
-        expect(response).to be_success
+        expect(response).to be_successful
         expect(assigns(:results)).to be_a(Array).and be_empty
         expect(assigns(:matched_words)).to be_a(Array).and be_empty
-        expect(controller).to set_flash.now[:info].to(/Enter a search term and repository name/)
+        expect(controller).to set_flash.now[:danger].to(/Enter at least one search term and repository name to get results/)
       end
     end
   end
 
-  context 'when params satisfy the search requirements' do
-    context 'but the repository was not found' do
-      it 'should return no results' do
-        get '/', params: { search_term: 'alias_method', repository_name: 'rails/railsssss'}
+  context "when params satisfy the search requirements" do
+    context "but the repository was not found" do
+      it "should return no results" do
+        get "/", params: {search_term: "alias_method", repository_name: "rails/railsssss", commit: "✔"}
 
-        expect(response).to be_success
+        expect(response).to be_successful
         expect(assigns(:results)).to be_a(Array).and be_empty
         expect(assigns(:matched_words)).to be_a(Array).and be_empty
         expect(controller).to set_flash.now[:danger].to(/The repository was not found/)
       end
     end
 
-    context 'but no matching code was found' do
-      it 'should return no results' do
-        get '/', params: { search_term: '%$^$&%*^&()**^&^%', repository_name: 'rails/rails'}
+    context "but no matching code was found" do
+      it "should return no results" do
+        get "/", params: {search_term: "あかさたなはまやらわアカサタナハマヤラワ", repository_name: "rails/rails", commit: "✔"}
 
-        expect(response).to be_success
+        expect(response).to be_successful
         expect(assigns(:results)).to be_a(Array).and be_empty
         expect(assigns(:matched_words)).to be_a(Array).and be_empty
         expect(controller).to set_flash.now[:warning].to(/No matching code was found/)
       end
     end
 
-    context 'and both the repository and matching code were found' do
-      it 'should return results' do
-        get '/', params: { search_term: 'alias_method', repository_name: 'rails/rails'}
+    context "and both the repository and matching code were found" do
+      it "should return results" do
+        get "/", params: {search_term: "alias_method", repository_name: "rails/rails", commit: "✔"}
 
-        expect(response).to be_success
+        expect(response).to be_successful
         expect(assigns(:results)).to be_a(Array).and be_present
         expect(assigns(:results).first.key?(:fragment)).to be_truthy
         expect(assigns(:results).first[:fragment]).to match(/alias_method/)
         expect(assigns(:results).first.key?(:html_url)).to be_truthy
-        expect(assigns(:results).first[:html_url]).to match(/https\:\/\/github.com\//)
+        expect(assigns(:results).first[:html_url]).to match(%r{https://github.com/})
         expect(assigns(:results).first.key?(:file_name)).to be_truthy
         expect(assigns(:results).first[:file_name]).to be_present
         expect(controller).not_to set_flash.now[:info]
